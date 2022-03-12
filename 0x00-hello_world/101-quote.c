8153484cc2ce487ec5d9ef1049bc369f800b3c21
Adeniyi Julius Olutade <adeniyijulius03@gmail.com>
2022-03-12 17:15:16 +0100

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- *
  * main - Entry point
  *
- * Return: 1 (Success)
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+
+int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
