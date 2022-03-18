803b5351abaf6105410e398a4afc31886f101278
Adeniyi Julius Olutade <adeniyijulius03@gmail.com>
2022-03-18 10:56:11 +0100

    new files

---
+++ 0x04-more_functions_nested_loops/3-print_numberrs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - prints number 0 - 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
