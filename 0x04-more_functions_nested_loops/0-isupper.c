82e002283ab9f1facd3398c4977d0b09c64db981
light-14 <69356412+light-14@users.noreply.github.com>
2022-03-18 11:14:27 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * _islower - checks if c is a lower case
- * @c: is the input value
- * Return: 1 if c is lower case. 0 otherwise
- */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
